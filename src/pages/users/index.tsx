fcd14e2c1a297a532f62600d1f8f38a966d74e5c
iagob28 <61669995+iagob28@users.noreply.github.com>
2022-03-24 14:11:40 -0300

    navigation

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,7 +5,6 @@ import {
   Flex,
   Heading,
   Icon,
-  Stack,
   Table,
   Tbody,
   Td,
@@ -15,6 +14,7 @@ import {
   Tr,
   useBreakpointValue,
 } from "@chakra-ui/react";
+import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header/index";
 import { Pagination } from "../../components/Pagination";
@@ -37,15 +37,17 @@ export default function UserList() {
             <Heading size="lg" fontWeight="normal">
               Usuários
             </Heading>
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-            >
-              Criar novo
-            </Button>
+            <Link href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+              >
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
 
           <Table colorScheme="whiteAlpha">
